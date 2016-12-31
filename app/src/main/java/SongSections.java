1149e9215d20d94e08538f957681ea9f893cf214
itayko99 <itayko.2000@gmail.com>
2016-12-30 22:59:56 -0400

    Still Nothing Clever, Still Depressed

--- app/src/main/java/SongSections.java
+++ app/src/main/java/SongSections.java
@@ -4,5 +4,22 @@
 
 public class SongSections {
 
+    String name;
+
+    /**
+     *
+     * @param n - Each Song section will be made with its name pre set as its main feature
+     */
+    public SongSections(String n){
+        name = n;
+    }
+
+    public void setName(String n){
+        name=n;
+    }
+
+    public String getName (){
+        return name;
+    }
 
 }
